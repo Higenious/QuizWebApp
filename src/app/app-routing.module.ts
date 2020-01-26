8f92d23e7d781d563d456328a7f8ed1c2a336ab4
Chetan tayade <cchetantayade@gmail.com>
2020-01-27 00:21:35 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {TeacherComponent} from '../app/components/teacher/teacher.component';
+import {StudentComponent} from '../app/components/student/student.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'teachers', component : TeacherComponent},
+  {path:'students', component : StudentComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
